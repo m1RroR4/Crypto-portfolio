5628d6f7317297ec0fc7c9ef6f7741b8148d5210
m1RroR4 <oliinykyurii123@gmail.com>
2024-08-01 19:38:51 +0200

    add TokenPurchase and TokenSale classes, change Token class structure

--- app/models.py
+++ app/models.py
@@ -2,13 +2,26 @@ from datetime import date, time
 from typing import Optional, List
 from pydantic import BaseModel, EmailStr, constr
 
-class Token(BaseModel):
-    symbol: str
+class TokenPurchase(BaseModel):
     amount: float
     total_cost: float
     purchase_price: float
     purchase_date: date
     purchase_time: Optional[time] = time(0, 0)
+
+class TokenSale(BaseModel):
+    amount: float
+    sale_price: float
+    sale_date: date
+    sale_time: Optional[time] = time(0, 0)
+
+class Token(BaseModel):
+    symbol: str
+    amount: float
+    total_cost: float
+    average_purchase_price: float
+    purchases: List[TokenPurchase] = []
+    sales: List[TokenSale] = []
 
 class User(BaseModel):
     username: constr(min_length=3, max_length=50)
